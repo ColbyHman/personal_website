cee82d28657da9d518109513e09049ef42c5614e
Colby Hillman <colby.hillman@gmail.com>
2024-02-05 21:59:19 -0500

    Added styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { HomeComponent } from './components/home/home.component';
+import { MatCardModule } from '@angular/material/card';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
     declarations: [
@@ -20,8 +22,11 @@ import { HomeComponent } from './components/home/home.component';
       AppRoutingModule, // Include your routing module
       CarouselModule.forRoot(),
       // Add other modules (FormsModule, HttpClientModule, etc.) as needed
+      MatCardModule,
     ],
-    providers: [],
+    providers: [
+    provideAnimationsAsync()
+  ],
     bootstrap: [AppComponent],
   })
   export class AppModule {}
